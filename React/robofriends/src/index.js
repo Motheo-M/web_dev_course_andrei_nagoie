28a64f086cb9ab4c03b30e9be48ed85970c46d7c
Motheo <motheotb@gmail.com>
2022-11-30 18:40:46 +0200

    update

--- React/robofriends/src/index.js
+++ React/robofriends/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 ReactDOM.render(
     <App />, document.getElementById('root')
