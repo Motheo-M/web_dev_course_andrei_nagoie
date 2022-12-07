dba7afe4ae59e2576263ee30b8d4fce984564491
Motheo <motheotb@gmail.com>
2022-12-07 20:37:39 +0200

    added searchbox

--- React/robofriends/src/App.js
+++ React/robofriends/src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList.js';
 import SearchBox from './SearchBox.js';
 import { robots } from './robots.js';
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={ robots }/>
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super()
+        this.state={
+            robots: robots,
+            searchfield: ''
+        }
+    }
+    render() {
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox />
+                <CardList robots={ this.state.robots } />
+            </div>
+        );
+    }
+    
 }
 
 export default App;
